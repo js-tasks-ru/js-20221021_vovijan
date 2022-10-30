e010c2fc18406ff1d82db2dfbb5ec25e9f3a1237
VGusar <vgusar@ibs.ru>
2022-10-30 12:50:55 +0300

    03 - Create createGetter function

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,17 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  const arrString = path.split('.');
 
+  return (obj) => {
+    if (!obj[arrString[0]]) {
+      return;
+    }
+
+    let newObj = {};
+
+    arrString.forEach((item, index) => index > 0 ? newObj = newObj[item] : newObj = obj[item]);
+
+    return newObj;
+  };
 }
