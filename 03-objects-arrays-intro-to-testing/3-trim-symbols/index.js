dca3724cd74148345220b6066900499711aa91eb
VGusar <vgusar@ibs.ru>
2022-10-30 19:29:21 +0300

    03 - Create trimSymbols function

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,35 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  if (size === undefined) {
+    return string;
+  }
 
+  if (string === '') {
+    return '';
+  }
+
+  const sizeStr = (a, b) => a.slice(0, b);
+
+  const arrStr = [...string];
+
+  const resultArr = [];
+  let str = '';
+
+  arrStr.forEach((item, index) => {
+    if ((str.length === 0) || (str.slice(-1) === item)) {
+      str = str.concat(item);
+    }
+
+    if (str.slice(-1) !== item) {
+      resultArr.push(sizeStr(str, size));
+      str = item;
+    }
+
+    if (arrStr.length === index + 1) {
+      resultArr.push(sizeStr(str, size));
+    }
+  });
+
+  return resultArr.join('');
 }
